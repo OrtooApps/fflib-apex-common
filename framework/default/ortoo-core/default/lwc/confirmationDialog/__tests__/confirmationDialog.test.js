62ea46af946377f577c6866247f3d2665b9f6cd2
Robert Baillie <robert.baillie@ortooapps.com>
2021-12-13 12:30:31 +0000

    Improving the testing of the confirmation dialog

--- framework/default/ortoo-core/default/lwc/confirmationDialog/__tests__/confirmationDialog.test.js
+++ framework/default/ortoo-core/default/lwc/confirmationDialog/__tests__/confirmationDialog.test.js
@@ -9,28 +9,46 @@ describe('c-confirmation-dialog', () => {
         }
     });
 
-    it('When set to visible, with a title, contains a div with the title, directing it to the title slot', () => {
+    it('When set to visible, with a title, contains a div with the title, directing it to the title slot of the modal', () => {
         const element = createElement('c-confirmation-dialog', {
             is: ConfirmationDialog
         });
-        element.title   = 'The title';
         element.visible = true;
+        element.title = 'A title';
         document.body.appendChild(element);
 
-        const expectedElement = element.shadowRoot.querySelector( 'c-modal div[slot="title"]' );
-        expect( expectedElement.innerHTML ).toBe( 'The title' );
+        return Promise.resolve()
+        .then( () => {
+            const expectedElement = element.shadowRoot.querySelector( 'c-modal div[slot="title"]' );
+            expect( expectedElement ).not.toBe( null );
+        });
     });
 
-    it('When set to visible, contains a div containing a message slot, directing it to the modal contents slot', () => {
+    it('When set to visible, contains a div containing a message slot, directing it to the contents slot of the modal', () => {
         const element = createElement('c-confirmation-dialog', {
             is: ConfirmationDialog
         });
-        element.message = 'The message';
         element.visible = true;
         document.body.appendChild(element);
 
         const expectedElement = element.shadowRoot.querySelector( 'c-modal div[slot="contents"]' );
         expect( expectedElement ).not.toBe( null );
+    });
+
+    it('When set to visible and no button labels provided contains a div containing cancel and confirm buttons with the defaulted labels, directing them to the modal footer slot', () => {
+        const element = createElement('c-confirmation-dialog', {
+            is: ConfirmationDialog
+        });
+        element.visible = true;
+        document.body.appendChild(element);
+
+        const expectedElement = element.shadowRoot.querySelector( 'c-modal div[slot="footer"]' );
+        expect( expectedElement ).not.toBe( null );
+
+        expect( expectedElement.querySelector( '[data-name="confirm"]' ).getAttribute( 'title' ) ).not.toBe( null );
+//        expect( expectedElement.querySelector( '[data-name="confirm"]' ).getAttribute( 'label' ) ).not.toBe( null );
+        expect( expectedElement.querySelector( '[data-name="cancel"]' ).getAttribute( 'title' ) ).not.toBe( null );
+//        expect( expectedElement.querySelector( '[data-name="cancel"]' ).getAttribute( 'label' ) ).not.toBe( null );
     });
 
     it('When set to visible, contains a div containing cancel and confirm buttons with the specified labels, directing them to the modal footer slot', () => {
@@ -45,11 +63,13 @@ describe('c-confirmation-dialog', () => {
         const expectedElement = element.shadowRoot.querySelector( 'c-modal div[slot="footer"]' );
         expect( expectedElement ).not.toBe( null );
 
-        expect( expectedElement.querySelector( '[title="Confirm"]' ) ).not.toBe( null );
-        expect( expectedElement.querySelector( '[title="Cancel"]' ) ).not.toBe( null );
+        expect( expectedElement.querySelector( '[data-name="confirm"]' ).getAttribute( 'title' ) ).toBe( element.confirmLabel );
+//        expect( expectedElement.querySelector( '[data-name="confirm"]' ).getAttribute( 'label' ) ).toBe( element.confirmLabel );
+        expect( expectedElement.querySelector( '[data-name="cancel"]' ).getAttribute( 'title' ) ).toBe( element.cancelLabel );
+//        expect( expectedElement.querySelector( '[data-name="cancel"]' ).getAttribute( 'label' ) ).toBe( element.cancelLabel );
     });
 
-    it('Clicking the confirm button will issue an event containing the confirm message', () => {
+    it('Clicking the confirm button will issue an event containing the confirm event message', () => {
 
         const CONFIRM_MESSAGE = 'The confirm message';
         const CANCEL_MESSAGE  = 'The cancel message';
@@ -58,10 +78,10 @@ describe('c-confirmation-dialog', () => {
             is: ConfirmationDialog
         });
 
-        element.confirmLabel   = 'Confirm';
-        element.confirmMessage = CONFIRM_MESSAGE;
-        element.cancelLabel    = 'Cancel';
-        element.cancelMessage  = CANCEL_MESSAGE;
+        element.confirmLabel        = 'Confirm';
+        element.confirmEventMessage = CONFIRM_MESSAGE;
+        element.cancelLabel         = 'Cancel';
+        element.cancelEventMessage  = CANCEL_MESSAGE;
 
         element.visible        = true;
         document.body.appendChild(element);
@@ -80,7 +100,7 @@ describe('c-confirmation-dialog', () => {
         expect( cancelHandler ).not.toHaveBeenCalled();
     });
 
-    it('Clicking the cancel button will issue an event containing the cancel message', () => {
+    it('Clicking the cancel button will issue an event containing the cancel event message', () => {
 
         const CONFIRM_MESSAGE = 'The confirm message';
         const CANCEL_MESSAGE  = 'The cancel message';
@@ -89,10 +109,10 @@ describe('c-confirmation-dialog', () => {
             is: ConfirmationDialog
         });
 
-        element.confirmLabel   = 'Confirm';
-        element.confirmMessage = CONFIRM_MESSAGE;
-        element.cancelLabel    = 'Cancel';
-        element.cancelMessage  = CANCEL_MESSAGE;
+        element.confirmLabel        = 'Confirm';
+        element.confirmEventMessage = CONFIRM_MESSAGE;
+        element.cancelLabel         = 'Cancel';
+        element.cancelEventMessage  = CANCEL_MESSAGE;
 
         element.visible        = true;
         document.body.appendChild(element);
